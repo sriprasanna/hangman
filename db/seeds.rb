c00753f9aeea4681f6cd077b4c8d6b9417450656
Sri Prasanna <k.sriprasanna@gmail.com>
2014-11-13 21:05:53 -0500

    adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Word.delete_all
+words_file = Rails.root.join('db', 'words.txt')
+
+open(words_file.to_path) do |words|
+  words.read.each_line do |word|
+    Word.create! text: word.strip
+  end
+end
